4006e9d6ce5c6965d450a829891ae605fe8e3fc5
root <root@localhost.localdomain>
2019-07-27 15:32:53 -1000

    ip range list as indexes

--- pysweep.py
+++ pysweep.py
@@ -1,39 +1,52 @@
 import subprocess
+import re
 
 
 
 #Global variable
 
 greeting = ["hello"]
+ip_target = "10.0.2.0/24"
+fping_range = "fping -g"
+# ip_range = ""
+pysweep_file = open("filePysweep.txt", mode ="w+t")
 
 
 
 #Create a list of IP addresses
 
 def create_ip_list():
-    pass
+    output = subprocess.run(['fping', '-g', '-a','-q', '10.0.2.0/24'], capture_output=True, check=False)
+    ip_range = output.stdout.split()
+    pysweep_file.write(str(ip_range).replace('b', ''))
+    # pysweep_file.write(str(output.stdout.split())
+    # print(ip_range)
+    
+    
 
-#Loop over the list of IP addresses
+    
+# Loop over the list of IP addresses
 def process_list():
-    pass
+    
+   pass
+    # output = subprocess.run(['fping', '-C', 5, ip_range], capture_output=True)
+    # print(output.stdout)
 
-#Print out each one
-def print_ip_addesess():
-    pass
+# #Print out each one
+# def print_ip_addesess():
+#     pass
 
-def add_on():
-    greeting.append("bye")  
-    print(greeting)
+# def add_on():
+#     greeting.append("bye")  
+#     print(greeting)
 
-def list_files():
-    output = subprocess.run(["fping", "10.0.2.6"], capture_output=True)
-    print(output.stdout, output)
+# def list_files():
+#     output = subprocess.run(["fping", "10.0.2.6"], capture_output=True)
+#     print(output.stdout, output.value)
 
 
 if __name__ == "__main__":
-    pass 
-    add_on()
-    print(greeting)
-    list_files()
-
+    create_ip_list()
+    process_list()
+    
   
