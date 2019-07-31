9bcb260b51706806c597cde0e27c3707be37ae66
root <root@localhost.localdomain>
2019-07-30 21:03:22 -1000

    Finally got information I need

--- pysweep.py
+++ pysweep.py
@@ -1,6 +1,6 @@
 import subprocess
 import re
-
+import os
 
 
 #Global variable
@@ -8,7 +8,7 @@ import re
 greeting = ["hello"]
 ip_target = "10.0.2.0/24"
 fping_range = "fping -g"
-ip_range = 0
+ip_range = []
 pysweep_file = open("filePysweep.txt", mode ="w+t")
 
 
@@ -16,47 +16,27 @@ pysweep_file = open("filePysweep.txt", mode ="w+t")
 #Create a list of IP addresses
 
 def create_ip_list():
-    output = subprocess.run(['fping', '-g', '-a','-q', '10.0.2.0/24'], capture_output=True, check=False)
-    ip_range = output.stdout.split()
-    pysweep_file.write(str(ip_range).replace('b', ''))
-    pysweep_file.close()
-    # pysweep_file.write(str(output.stdout.split())
-    # print(ip_range)
-    # sorted(pysweep_file)
-    # print(pysweep_file)
-    print(ip_range)
-    return pysweep_file
-    
+       
+     output = subprocess.run(['fping', '-g','-a','-q', ip_target], text = True, capture_output=True)
+     spl_output = output.stdout.split('\n')
+
+     for ip in spl_output:
+             output = subprocess.run(['fping', '-C', '5', str(ip)], capture_output=True, text = True)
+             pysweep_file.write(output.stdout)   
+     
+     print(ip_range)
+     print("resources")
 
     
 # Loop over the list of IP addresses
 def process_list():
-    def loop_over():
-        for ip in pysweep_file:
-            return ip
-        print(ip)
-    # output = subprocess.run(['fping', '-C', '5', loop_over()], capture_output=True)
-    
-
-   
-    # output = subprocess.run(['fping', '-C', 5, ip_range], capture_output=True)
-    # print(output.stdout)
-
-# #Print out each one
-# def print_ip_addesess():
-#     pass
-
-# def add_on():
-#     greeting.append("bye")  
-#     print(greeting)
-
-# def list_files():
-#     output = subprocess.run(["fping", "10.0.2.6"], capture_output=True)
-#     print(output.stdout, output.value)
-
+    print('this is a string', ip_range)
+    for i in ip_range:
+        print('fart',i)
+            
 
 if __name__ == "__main__":
     create_ip_list()
-    process_list()
-    print(ip_range)
+    #process_list()
+    print('this is out',ip_range)
   
